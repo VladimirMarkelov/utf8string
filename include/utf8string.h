8b6b469448fbb4e8a3c8a2bb045b814e9f834a7d
Vladimir Markelov <vladimir.markelov@nexenta.com>
2017-01-30 15:04:34 -0800

    simplify operations

--- include/utf8string.h
+++ include/utf8string.h
@@ -1,17 +1,34 @@
-struct utf8string;
-
-#define UTF8_INITIAL_BUFFER 64
-
 /* Basic operations */
-struct utf8string * utf8str_new();
-void utf8str_free(struct utf8string *str);
-struct utf8string * utf8str_from_char(const char *str);
-const char * utf8str_to_char(const struct utf8string *str);
-struct utf8string * utf8str_duplicate(const struct utf8string *str);
-void utf8str_clear(struct utf8string *str);
-void utf8str_reset(struct utf8string *str);
-int utf8str_set_capacity(struct utf8string *str, size_t new_capacity);
-size_t utf8str_capacity(const struct utf8string *str);
-size_t utf8str_used(const struct utf8string *str);
-
-size_t utf8str_len(const struct utf8string *str);
+size_t utf8str_len(const char *str);
+/*
+str_cat - char & char*
+is_valid - check if it contains valid utf
+count - number of utf8 symbols
+len - number of bytes used
+delete - char, substring, and (from, to)
+insert - substring and char
+append - substring and char
+at - utf8 symbol at position
+upcase
+lowcase
+compare
+compare_no_case
+utf8width - for the object, char, char*
+utf8width_first - first char of char*
+substring
+str_srch - cp, char & char*
+starts_with
+end_with
+replace
+trim
+trim_right
+trim_left
+back - find the first byte of the symbol under pointer
+last - go to last symbol
+is_digit
+is_whitespace
+is_letter
+is_letter_upcase
+is_letter_lowcase
+is_punct
+*/
--- tests/utf8test.c
+++ tests/utf8test.c
@@ -9,41 +9,10 @@ int tests_run = 0;
 int tests_fail = 0;
 
 const char* test_basic() {
+    /*
     struct utf8string *ustr = utf8str_new();
     ut_assert("New", ustr != NULL && utf8str_to_char(ustr) != NULL);
-
-    utf8str_free(ustr);
-    ut_assert("Free", 1);
-
-    ustr = utf8str_from_char("test");
-    ut_assert("From char",
-            ustr != NULL &&
-            utf8str_to_char(ustr) != NULL &&
-            strcmp(utf8str_to_char(ustr), "test") == 0 &&
-            utf8str_capacity(ustr) == UTF8_INITIAL_BUFFER &&
-            utf8str_used(ustr) == 4);
-
-    utf8str_set_capacity(ustr, 128);
-    ut_assert("Resize",
-            utf8str_to_char(ustr) != NULL &&
-            strcmp(utf8str_to_char(ustr), "test") == 0 &&
-            utf8str_capacity(ustr) == 128 &&
-            utf8str_used(ustr) == 4);
-
-    utf8str_clear(ustr);
-    ut_assert("Clear",
-            utf8str_to_char(ustr) != NULL &&
-            strlen(utf8str_to_char(ustr)) == 0 &&
-            utf8str_capacity(ustr) == 128 &&
-            utf8str_used(ustr) == 0);
-
-    utf8str_reset(ustr);
-    ut_assert("Reset",
-            utf8str_to_char(ustr) != NULL &&
-            strlen(utf8str_to_char(ustr)) == 0 &&
-            utf8str_capacity(ustr) == UTF8_INITIAL_BUFFER &&
-            utf8str_used(ustr) == 0);
-
+    */
     return 0;
 }
 
